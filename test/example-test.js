da57e58e3e917422083dcb1977020b0ec17e13c2
Christopher Bebry <invalidsyntax@gmail.com>
2013-05-30 22:29:53 -0400

    Added example test template.

---
+++ test/example-test.js
@@ -0,0 +1,11 @@
+/*
+var assert = require('assert');
+ 
+describe('TheObject', function() {
+	describe('#aFunction()', function() {
+		it('should have some demonstrable charactistics', function(done) {
+			//assert stuff and done() when done
+		});
+	});
+});
+*/
