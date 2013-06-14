5ac6579cc6960fccf4bb5a10532c227f4bd0c3ae
Christopher Bebry <invalidsyntax@gmail.com>
2013-06-14 00:17:46 -0400

    Backend register implementation.

--- index.js
+++ index.js
@@ -4,10 +4,33 @@ var serverConfig = require('./config/config').config,
     server = new Hapi.Server(serverConfig.hostname, serverConfig.port, options);
 
 var util = require('./lib').Util;
+var auth = require('./lib').Auth;
+
+server.auth('session', {
+    scheme: 'cookie',
+    password: 'sdoi239fsER0a1', //TODO: refactor this out to gitignored auth config file
+    cookie: 'cartography-cookie',  //?TODO: refactor this out to gitignored auth config file
+    redirectTo: '/',
+	isSecure: false, //TODO: generate SSL cert, put together config for this
+	ttl: 1800000,
+	clearInvalid: true
+});
+
+
 server.route([
   { method: 'GET', path: '/version', handler: function() { this.reply(util.version); } },
+  
+  { method: 'GET', path: '/confirm/{hashkey}', config: { handler: auth.confirm, auth: false  } },
+  { method: 'POST', path: '/register', config: { handler: auth.register, validate: { payload: auth.register_validate(Hapi) }, auth: false  } },
+  
   { method: '*', path: '/{path*}', handler: { directory: { path: './static/', listing: false, redirectToSlash: true } } }
 ]);
 
-server.start();
-console.log('Server up at ' + server.info.uri + ' !');
+
+var db = require('./lib/models');
+db.init(function() {
+	console.log('database setup complete');
+	server.start();
+	console.log('Server up at ' + server.info.uri + ' !');
+});
+
