827a7d1ec4776dc36d2544261488ded2e0f165c8
Christopher Bebry <invalidsyntax@gmail.com>
2013-06-15 00:40:29 -0400

    Registration, email sends out. Confirmation parses properly now with the slash handling. Need to do something with confirmation next.

--- config/config.example.js
+++ config/config.example.js
@@ -10,3 +10,11 @@ exports.config = {
   hostname: '0.0.0.0',
   port: 8000
 };
+
+exports.mailconfig = {
+  method: 'sendmail',
+  sendmail: {
+    bin: '/usr/sbin/sendmail',
+	from: '"Cartography Server" <no-reply@something.com>'
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var Hapi = require('hapi'),
     options = { };
-var serverConfig = require('./config/config').config,
+var masterConfig = require('./config/config');
+var serverConfig = masterConfig.config,
     server = new Hapi.Server(serverConfig.hostname, serverConfig.port, options);
 
 var util = require('./lib').Util;
 var auth = require('./lib').Auth;
+var mailer = require('./lib').Mailer;
+var mailConfig = masterConfig.mailconfig;
+mailer.init(mailConfig);
 
 server.auth('session', {
     scheme: 'cookie',
@@ -20,7 +24,7 @@ server.auth('session', {
 server.route([
   { method: 'GET', path: '/version', handler: function() { this.reply(util.version); } },
   
-  { method: 'GET', path: '/confirm/{hashkey}', config: { handler: auth.confirm, auth: false  } },
+  { method: 'GET', path: '/confirm/{hashkey*2}', config: { handler: auth.confirm, auth: false  } },
   { method: 'POST', path: '/register', config: { handler: auth.register, validate: { payload: auth.register_validate(Hapi) }, auth: false  } },
   
   { method: '*', path: '/{path*}', handler: { directory: { path: './static/', listing: false, redirectToSlash: true } } }
