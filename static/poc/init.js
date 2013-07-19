a6e8fad6e4f0dcd7dc51926e390f65b479515ba6
Christopher Bebry <invalidsyntax@gmail.com>
2013-07-19 01:26:38 -0400

    Playing around with three.js plane geometries.

--- static/poc/init.js
+++ static/poc/init.js
@@ -1,15 +1,20 @@
 // revolutions per second
-var angularSpeed = 0.2; 
-var lastTime = 0;
+//var angularSpeed = 0.2; 
+//var lastTime = 0;
+
+var VIEW_ANGLE = 45;
+var ASPECT = window.innerWidth / window.innerHeight;
+var NEAR = 1;
+var FAR = 1000;
 
 // this function is executed on each animation frame
 function animate(){
 	// update
-	var time = (new Date()).getTime();
-	var timeDiff = time - lastTime;
-	var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 1000;
-	plane.rotation.z += angleChange;
-	lastTime = time;
+	//var time = (new Date()).getTime();
+	//var timeDiff = time - lastTime;
+	//var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 1000;
+	//plane.rotation.z += angleChange;
+	//lastTime = time;
 
 	// render
 	renderer.render(scene, camera);
@@ -26,7 +31,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // camera
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 camera.position.y = -450;
 camera.position.z = 400;
 camera.rotation.x = 45 * (Math.PI / 180);
@@ -35,9 +40,10 @@ camera.rotation.x = 45 * (Math.PI / 180);
 var scene = new THREE.Scene();
 
 // plane
-var plane = new THREE.Mesh(new THREE.PlaneGeometry(300, 300), new THREE.MeshNormalMaterial());
-plane.overdraw = true;
-scene.add(plane);
+var plane = new THREE.PlaneGeometry(300, 300);
+var planemesh = new THREE.Mesh(plane, new THREE.MeshNormalMaterial());
+planemesh.overdraw = true;
+scene.add(planemesh);
 
 // start animation
 animate();
